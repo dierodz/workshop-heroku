1007e59e896d5771ce07bac0b9c6e445166e498b
Diego Rodriguez <dierodz91@gmail.com>
2021-01-14 15:34:58 -0300

    product route

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -8,5 +8,32 @@ server.get('/', (req, res, next) => {
 		})
 		.catch(next);
 });
+server.post("/", (req, res, next) => {
+  Product.create(req.body)
+    .then((product) => {
+      res.send(product);
+    })
+    .catch(next);
+});
+server.put("/:id", (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      return product.update(req.body);
+    })
+    .then((product) => {
+      res.send(product);
+    })
+    .catch(next);
+});
+server.delete("/:id", (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((product) => {
+      res.send(product);
+    })
+    .catch(next);
+});
 
 module.exports = server;
