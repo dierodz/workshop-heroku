06c67990ff085cd389116c55c0f0609baeebc1a9
Diego Rodriguez <dierodz91@gmail.com>
2021-01-14 14:38:31 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -7,19 +7,24 @@ const routes = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
+const cors = require("cors");
 
 server.name = 'API';
 
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
-});
+};);
 
 server.use('/', routes);
 
